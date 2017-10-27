9fa500249b18d0cd3c86695519b41dae700d6a91
Михаил Лямаев <misha25_live@mail.ru>
2017-10-28 09:18:57 +1000

    initial commit

---
+++ ArduinoRobotics2017/ArduinoRobotics2017/ArduinoRobotics2017.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoRobotics2017.ino
+ Created:	28.10.2017 9:18:31
+ Author:	misha
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
