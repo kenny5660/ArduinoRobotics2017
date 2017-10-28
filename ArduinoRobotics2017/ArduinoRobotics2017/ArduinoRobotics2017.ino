487c5383ae7a84843c652b95ddb4bdbf5122f21a
Михаил Лямаев <misha25_live@mail.ru>
2017-10-28 16:43:05 +1000

    Button working

--- ArduinoRobotics2017/ArduinoRobotics2017/ArduinoRobotics2017.ino
+++ ArduinoRobotics2017/ArduinoRobotics2017/ArduinoRobotics2017.ino
@@ -8,26 +8,44 @@
 #include <SparkFun_APDS9960.h>
 #define RLINESEN A2 //правый датчик линии
 #define LLINESEN A3 //левый датчик линии
+#define BUT1 A1
+#define BUT2 A0
 // the setup function runs once when you press reset or power the board
 #define P   0.3 
 #define I 0     
 #define D 40 
 Servo servoBack;
+int mode = 0;
 void setup() {
 	Serial.begin(9600);
+
 	pinMode(13, OUTPUT);
 	pinMode(LLINESEN, INPUT);
 	pinMode(RLINESEN, INPUT);
+	pinMode(BUT1,INPUT_PULLUP);
+	pinMode(BUT2, INPUT_PULLUP);
 	MotorInit();//инцилизация моторов
-	//MotorRight(-100);
-	//MotorLeft(100);
-//	servoBack.attach(10);
-//	servoBack.write(0);
+	Adps_init();//дачик цвета
+	MotorRight(0);
+	MotorLeft(0);
+	servoBack.attach(10);
+	servoBack.write(0);
+	while (1) {
+		if (digitalRead(BUT1) == 0) {
+			mode = 1;
+			break;
+		}
+		if (digitalRead(BUT2) == 0)
+		{
+			mode = 2;
+			break;
+		}
+   }
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	black_line(P, I, D);
-	//Serial.println(analogRead(LLINESEN));
-	
+	//black_line(P, I, D);
+	//Serial.println(mode);
+	Adps_Debug();
 }
